d65c0cd07b4f909a8da70ef43e84942f0294da63
Eldar Khusainov <elderyendex@yandex.ru>
2022-11-09 10:45:49 +0000

    added nojekyll

---
+++ script.js
@@ -0,0 +1,40 @@
+const openPopupButton = document.querySelector('.profile__button-edit');
+const closePopupButton = document.querySelector('.popup__close-button');
+const popupForm = document.querySelector('.popup__form');
+const formElement = document.querySelector('.popup');
+const inputName = document.querySelector('#name');
+const inputJob = document.querySelector('#job');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__occupation');
+const inputNameValue = document.querySelector('#name').value = profileName.textContent;
+const inputJobValue = document.querySelector('#job').value = profileJob.textContent;
+const submitButton = document.querySelector('#submitid');
+
+openPopupButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  popupForm.classList.add('active');
+  formElement.classList.add('active');
+});
+
+closePopupButton.addEventListener('click', () => { 
+  popupForm.classList.remove('active'); 
+  formElement.classList.remove('active'); 
+});
+
+submitButton.addEventListener('click', () => { 
+  popupForm.classList.remove('active'); 
+  formElement.classList.remove('active'); 
+});
+
+const formSubmitHandler = (evt) => {
+  evt.preventDefault();
+
+  const nameValue = inputName.value;
+  const jobValue = inputJob.value;
+  profileName.textContent = nameValue;
+  profileJob.textContent = jobValue;
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+
+
