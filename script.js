a32524190d115f3a1261ca4c694c1fa5ffbc33b6
Eldar Khusainov <elderyendex@yandex.ru>
2022-11-22 15:15:32 +0000

    3rd itteration

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const popupElement = document.querySelector('.popup');
 const popupCloseElement = popupElement.querySelector('.popup__close-button');
 const popupOpenElement = document.querySelector('.profile__button-edit');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
+const inputNameValue = document.querySelector('#inputName');
+const inputJobValue = document.querySelector('#inputJob');
 
 const openPopup = function () {
   popupElement.classList.add('popup_is-opened');
+  inputNameValue.value = profileName.textContent;
+  inputJobValue.value = profileJob.textContent;
 }
 const closePopup = function () {
   popupElement.classList.remove('popup_is-opened');
@@ -12,20 +18,13 @@ const closePopup = function () {
 popupOpenElement.addEventListener('click', openPopup);
 popupCloseElement.addEventListener('click', closePopup);
 
-const profileName = document.querySelector('.profile__name');
-const profileJob = document.querySelector('.profile__job');
-const inputNameValue = document.querySelector('#inputName').value = profileName.textContent;
-const inputJobValue = document.querySelector('#inputJob').value = profileJob.textContent;
-
-const formElement = document.querySelector('.popup__form')
+const formElement = document.querySelector('.popup__form');
 
 const formSubmitHandler = (evt) => {
   evt.preventDefault();
 
-  const nameValue = inputName.value;
-  const jobValue = inputJob.value;
-  profileName.textContent = nameValue;
-  profileJob.textContent = jobValue;
+  profileName.textContent = inputNameValue.value;
+  profileJob.textContent = inputJobValue.value;
 
   popupElement.classList.remove('popup_is-opened');
 }
